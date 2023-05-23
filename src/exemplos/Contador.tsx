26ec6e600eee2ed4aadcabc920657f3b908e3aa3
Helio <heliohenrique3@gmail.com>
2023-05-23 10:07:51 -0300

    Passando exemplo do contador para componente separado

---
+++ src/exemplos/Contador.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './Contador.css'
+
+export function Contador() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Contador</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
